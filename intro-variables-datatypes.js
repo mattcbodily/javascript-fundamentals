3b097b5ef2cd1156b2b6c57d7e39018cb3e9c110
Matthew Bodily <mattcbodily@gmail.com>
2019-06-13 23:28:43 -0600

    Intro text added

---
+++ intro-variables-datatypes.js
@@ -0,0 +1,13 @@
+// Hello and welcome to Javascript Fundamentals! This is a beginner level course to learn the fundamental concepts of the Javascript
+// language. These lessons will be broken into two different sections: An concept explanation section, and a problem solving section.
+// The concept explanation section will involve going over specific Javascript concepts and syntax, while the problem solving section
+// will give you an opportunity to practice what you have learned.  These problems will be offered in a drill type format, providing
+// many problems for each topic, as this will provide necessary repetition to help remember the concept and it's syntax. Let's begin!
+
+
+
+// Lesson 1: Introduction to Javascript, Variables, and Data Types
+
+//What is Javascript?
+// Javascript is an object-oriented programming language that is used for web development. Javascipt is generally used to enhance a web
+// page to provide a user friendly experience.
