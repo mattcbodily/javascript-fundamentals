4c706483f0243dd186dd90f1bdbad56802230181
Matthew Bodily <mattcbodily@gmail.com>
2019-06-13 23:42:21 -0600

    added variables section

--- intro-variables-datatypes.js
+++ intro-variables-datatypes.js
@@ -8,6 +8,25 @@
 
 // Lesson 1: Introduction to Javascript, Variables, and Data Types
 
-//What is Javascript?
+//What is Javascript? <== Add more to this section
 // Javascript is an object-oriented programming language that is used for web development. Javascipt is generally used to enhance a web
 // page to provide a user friendly experience.
+
+
+//What is a Variable?
+//The Concept:
+// A variable is a way of storing information in Javascript code.  You can think of a variable as a box. That box can contain many 
+// different, which I will cover shortly. Here are some examples of variables:
+
+//The Syntax:
+var name = "Christopher";
+var age = 26;
+var hungry = true;
+
+//Syntax Breakdown:
+// Above you will see the var keyword. This keyword is how we define, or create, a variable.  Following the var keyword, we add the name 
+// of the variable; In the cases of above, I have named the variables name, age, and hungry respectively. Following the variable name, 
+// there is an equal sign, which is called the assignment operator in Javascript. To the right of the assignment operator is the value 
+// we want to store to the variable.
+
+//What are Data types?
