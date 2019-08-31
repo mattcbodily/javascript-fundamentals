f97dfef25d29932af539ade4e4177d5b05f17f63
Matthew Bodily <mattcbodily@gmail.com>
2019-08-31 00:04:56 -0600

    added the necessary html files to use jasmine testing. Jasmine specrunner now shows up in browser

---
+++ Test/lessonOneSpec.js
@@ -0,0 +1,11 @@
+describe('Jasmine Test', ()=>{
+    it('Variable should exist', ()=>{
+      //...
+    })
+    it('should delete an item', ()=>{
+      //...
+    })
+    it('should mark item as complete', ()=>{
+      //...
+    })
+  })
